55c81b25d8df661893622992d530ef6a0d4e673d
Juanito970 <juan.angel19@hotmail.com>
2020-10-30 16:01:42 -0600

    Inverse ussing gauss

--- inverseGauss.py
+++ inverseGauss.py
@@ -1,5 +1,4 @@
-import numpy as np
-
+#import numpy as np
 def inverseUsingGauss(*a):
     a = np.matrix(a[0])
     dimensions = np.shape(a)
@@ -15,7 +14,6 @@ def inverseUsingGauss(*a):
     n = nCol
     iMatrix = np.eye(n) #The identity matrix to append
     augmentedMatrix = np.c_[a, iMatrix]
-    print(augmentedMatrix)
     #Gauss: Generate an upper triangular matrix
     for j in range(n - 1):
         pivot = augmentedMatrix[j, j]
@@ -27,16 +25,13 @@ def inverseUsingGauss(*a):
     #Gauss-Jordan: Generates the inverse 
     for j in range(n - 1, -1, -1):
         pivot = augmentedMatrix[j,j]
-        for i in range(j, -1, -1):
+        for i in range(j - 1, -1, -1):
             factor = augmentedMatrix[i, j]
             if factor == 0:
                 continue
-            augmentedMatrix[i-1] = augmentedMatrix[i-1] + augmentedMatrix[j]*pivot/(-factor)
-    print(augmentedMatrix)
+            augmentedMatrix[i] = augmentedMatrix[j] + augmentedMatrix[i]*pivot/(-factor)
+        augmentedMatrix[j] = augmentedMatrix[j]/augmentedMatrix[j,j]
+    return augmentedMatrix[:,n:]
 
-
-
-
-matrixToSolve =  np.matrix([[1, -1, 0, 3], [2, 1, 2, 1], [2, -3, -2, 0], [0, 3, -2, 4]])
-
-print(inverseUsingGauss(matrixToSolve))
+#matrixToSolve =  np.matrix([[2,3,4], [1,1,1], [2,1,1]])
+#print(inverseUsingGauss(matrixToSolve))
