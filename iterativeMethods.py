42c464b7d2df924462c72a5e31e57ae3f1a08cf6
Juanito970 <juan.angel19@hotmail.com>
2020-10-30 19:57:10 -0600

    Problem finished

--- iterativeMethods.py
+++ iterativeMethods.py
@@ -1,10 +1,10 @@
-#import numpy as np
+import numpy as np
+
 def inverseUsingGauss(*a):
     a = np.matrix(a[0])
     dimensions = np.shape(a)
     nCol = dimensions[1]
     nRows = dimensions[0]
-    
     if nRows != nCol:
         print("Matrix is not square, this algorithm does not work! :(")
         return None
@@ -33,5 +33,26 @@ def inverseUsingGauss(*a):
         augmentedMatrix[j] = augmentedMatrix[j]/augmentedMatrix[j,j]
     return augmentedMatrix[:,n:]
 
-#matrixToSolve =  np.matrix([[2,3,4], [1,1,1], [2,1,1]])
-#print(inverseUsingGauss(matrixToSolve))
+def solveUssingIterative(a, b, relaxation = 1, maxIterations = 100, TOL = 0.0001):
+    b = np.matmul(a.T, b)
+    a = np.matmul(a.T, a)
+    rows = np.shape(a)[0]
+    x0 = np.ones(rows)
+    d = relaxation*np.diag(a)
+    errorNorm = 100
+    currentIteration = 0
+    while errorNorm > TOL and currentIteration < maxIterations:
+        for _ in range(30):
+            x1 = x0 + (b - np.matmul(a, x0))/d
+            errorNorm = (x0 - x1)/x1
+            errorNorm = np.linalg.norm(errorNorm)
+            x0 = x1
+            currentIteration += 1
+    if errorNorm > TOL and currentIteration > maxIterations:
+        print("The algorithm not converged, try setting more iterations, lowering error or incressing relaxation parameter")
+        return none
+    return x1
+
+# A = np.array([[1, -1, 0, 3], [2, 1, 2, 1], [2, -3, -2, 0], [0, 3, -2, 4]])
+# B = np.array([8 ,11, -8, 14])
+# print(solveUssingIterative(A, B))
